937e2b562a45faaf315f0781fab0c16a3888b1c1
jythor0008 <thorjiaying0008@gmail.com>
2026-09-10 02:23:14 +0800

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        a = [0] * 26
+        for i in range(len(s)):
+            a[ord(s[i]) - ord('a')] += 1;
+            a[ord(t[i]) - ord('a')] -= 1;
+
+        for i in range(26):
+            if a[i] != 0:
+                return False
+
+        return True
+
+        
