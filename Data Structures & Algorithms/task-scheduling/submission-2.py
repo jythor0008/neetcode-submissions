a396c6809be8e27c3aabde2dd5ce8dc98b455d8b
jythor0008 <thorjiaying0008@gmail.com>
2026-09-17 16:31:38 +0800

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        max_heap = [-v for v in Counter(tasks).values()]
+
+        heapq.heapify(max_heap)
+
+        # Now i have a maxheap of all the counts, i want to loop it until im done
+        cpu = 0
+        q = deque()
+        while max_heap or q:
+            # Add any ready thing into the queue
+            if not max_heap:
+                cpu = q[0][0]
+                
+            while q and q[0][0] == cpu:
+                heapq.heappush(max_heap, q.popleft()[1])
+
+            cur = heapq.heappop(max_heap)
+            cur += 1
+            if cur < 0:
+                q.append((n + cpu + 1, cur))
+
+            cpu += 1
+        
+        return cpu
+
+            
+            
+
+
