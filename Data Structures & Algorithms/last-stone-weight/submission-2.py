7e88e777cb9d36f5723ae81d545295f503bfa803
jythor0008 <thorjiaying0008@gmail.com>
2026-09-17 16:08:33 +0800

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        max_heap = []
+        for stone in stones:
+            heapq.heappush(max_heap, -stone)
+
+        while len(max_heap) > 1:
+            stone1 = -heapq.heappop(max_heap)
+            stone2 = -heapq.heappop(max_heap)
+
+            if stone1 == stone2:
+                continue
+
+            heapq.heappush(max_heap, -(stone1- stone2))
+
+        return -max_heap[0] if max_heap else 0
